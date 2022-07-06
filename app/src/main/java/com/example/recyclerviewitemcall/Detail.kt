4ad7226ac2553645d88ad73968616449dd1128d9
jeongmin <tmxk0810@gmail.com>
2022-07-06 15:54:25 +0900

    코드 정리

--- app/src/main/java/com/example/recyclerviewitemcall/Detail.kt
+++ app/src/main/java/com/example/recyclerviewitemcall/Detail.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_detail.*
 
--- app/src/main/java/com/example/recyclerviewitemcall/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewitemcall/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.example.recyclerviewitemcall
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val TAG : String = "jeongmin"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,23 +22,5 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = RecyclerUserAdapter(list)
         listUser.adapter = adapter
-    }
-    // permission 체크
-    private fun checkPermission() {
-        val status = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE)
-        if(status == PackageManager.PERMISSION_GRANTED) { // 권한 있을 경우
-            Log.d(TAG, "permission granted")
-        } else if(status == PackageManager.PERMISSION_DENIED){
-            requestPermission()
-            Log.d(TAG, "permission denied")
-        }
-    }
-    // permission 요청
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 99)
-    }
-
-    fun onClickCall(view: View) {
-
     }
 }
